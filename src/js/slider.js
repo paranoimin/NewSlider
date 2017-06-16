5a5e2959a30b71f443ddaa6a2176fe081bf7d920
Jamin <paranoimin@gmail.com>
2017-06-16 16:15:48 +0900

    stud complite

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,47 +1,70 @@
-var winWidth;
+var winWidth, studs;
 var pos = 0;
 var items = $("#mold li").length;
-
-// var winWidth = $(window).width();
 
 $(function() {
   items = $("#mold li").length;
   winWidth = $(window).width();
-  //console.log(items);
+  studs =  "<ul id='studs'>";
+  for (var i = 0; i < items; i++) {
+    studs +=  "<li class='studsInner'>";
+    studs +=    "<a class="+[i]+" href='javascript:void(0)' title='studs'>";
+    studs +=    "</a>";
+    studs +=  "</li>";
+  }
+  studs += "</ul>";
+  $("#studArea").html(
+    studs
+  );
+  $("#studs li a.0").css({"backgroundColor":"tomato"});
+
   sizReset(winWidth);
 
   $(window).resize(function () {
     winWidth = $(window).width();
     sizReset(winWidth, pos);
-    console.log(pos);
   });
 
   $("#nextBtn").on("click", function() {
-    console.log(pos);
     winWidth = $(window).width();
-
+    $("#studs li a").css({"background":"none"});
     if(pos === items-1) {
       pos = 0;
-      $("#mold:not(:animated)").stop().animate({"marginLeft":"0px"}, 300, "swing", function() {
-
-      });
+      $("#mold:not(:animated)").stop().animate({"marginLeft":"0px"}, 300, "swing");
+      $("#studs li a."+pos).css({"backgroundColor":"tomato"});
     } else {
       pos++;
       nextMove(winWidth, pos);
+      $("#studs li a."+pos).css({"backgroundColor":"tomato"});
     }
   });
-  $("#prevBtn").on("click", function() {
-    console.log(pos);
-    winWidth = $(window).width();
 
+  $("#prevBtn").on("click", function() {
+    winWidth = $(window).width();
+    $("#studs li a").css({"background":"none"});
     if(pos === 0) {
       pos = items-1;
-      $("#mold:not(:animated)").stop().animate({"marginLeft":"-"+winWidth*pos+"px"}, 300, "swing", function() {
-
-      });
+      $("#mold:not(:animated)").stop().animate({"marginLeft":"-"+winWidth*pos+"px"}, 300, "swing");
+      $("#studs li a."+pos).css({"backgroundColor":"tomato"});
     } else {
       pos--;
       prevMove(winWidth, pos);
+      $("#studs li a."+pos).css({"backgroundColor":"tomato"});
+    }
+  });
+
+  $("#studs li a").click(function() {
+    winWidth = $(window).width();
+    $("#studs li a").css({"background":"none"});
+    $(this).css({"backgroundColor":"tomato"});
+    if($(this).attr("class") > pos) {
+      pos = Number($(this).attr("class"));
+      $("#mold:not(:animated)").stop().animate({"marginLeft":"-"+winWidth*pos+"px"}, 300, "swing");
+    } else if($(this).attr("class") < pos) {
+      pos = Number($(this).arrt("class"));
+      $("#mold:not(:animated)").stop().animate({"marginLeft":winWidth*pos+"px"}, 300, "swing");
+    } else {
+
     }
   });
 
@@ -51,15 +74,10 @@ function sizReset(winSize, pos) {
   $("#sliderArea").css({"height":winSize/2+"px"});
   $("#mold").css({"width":winSize*items+"px","height":winSize/2+"px","marginLeft":"-"+winSize*pos+"px"});
   $("#mold li").css({"width":winSize+"px","height":winSize/2+"px"});
-
 }
 function nextMove(widSize, pos) {
-  $("#mold:not(:animated)").stop().animate({"marginLeft":"-="+widSize+"px"}, 300, "swing", function() {
-
-  });
+  $("#mold:not(:animated)").stop().animate({"marginLeft":"-="+widSize+"px"}, 300, "swing");
 }
 function prevMove(widSize, pos) {
-  $("#mold:not(:animated)").stop().animate({"marginLeft":"+="+widSize+"px"}, 300, "swing", function() {
-
-  });
+  $("#mold:not(:animated)").stop().animate({"marginLeft":"+="+widSize+"px"}, 300, "swing");
 }
